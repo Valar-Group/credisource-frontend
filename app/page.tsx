cf4444b365fca9c4e7210d0fd35fcc5880f8a9a3
Valar-Group <jk@valar.uk>
2025-10-31 22:52:33 +0000

    Remove duplicate NewsIcon function

--- app/page.tsx
+++ app/page.tsx
@@ -292,14 +292,6 @@ function TextIcon() {
   );
 }
 
-function NewsIcon() {
-  return (
-    <svg className="w-10 h-10 mx-auto" fill="currentColor" viewBox="0 0 24 24">
-      <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>
-    </svg>
-  );
-}
-
 // Feature Icons (Flat, Professional Style)
 function MultiProviderIcon() {
   return (
@@ -323,14 +315,6 @@ function TrustScoreIcon() {
   return (
     <svg className="w-12 h-12 text-[#E91E8C]" fill="currentColor" viewBox="0 0 24 24">
       <path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm-2 16l-4-4 1.41-1.41L10 14.17l6.59-6.59L18 9l-8 8z"/>
-    </svg>
-  );
-}
-
-function NewsIcon() {
-  return (
-    <svg className="w-10 h-10 mx-auto" fill="currentColor" viewBox="0 0 24 24">
-      <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>
     </svg>
   );
 }
